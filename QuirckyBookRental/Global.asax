1e516b2945aa41bd2135a5fa2efc411933d1734e
Norewal <63234816+Norewal@users.noreply.github.com>
2022-02-17 18:37:47 +0100

    Add project files.

---
+++ QuirckyBookRental/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuirckyBookRental.MvcApplication" Language="C#" %>
---
+++ QuirckyBookRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuirckyBookRental.Startup))]
+namespace QuirckyBookRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
