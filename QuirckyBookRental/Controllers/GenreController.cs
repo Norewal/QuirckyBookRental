ed3b0a2444ac8cabb9fb32f22f5a2fd22a1c8c5f
Norewal <63234816+Norewal@users.noreply.github.com>
2022-03-01 18:33:53 +0100

    Adding Edit & Delete part to Genre

--- QuirckyBookRental/Controllers/GenreController.cs
+++ QuirckyBookRental/Controllers/GenreController.cs
@@ -1,6 +1,7 @@
 ﻿using QuirkyBookRental.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -35,7 +36,7 @@ namespace QuirkyBookRental.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Genre genre)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 db.Genres.Add(genre);
                 db.SaveChanges();
@@ -45,7 +46,7 @@ namespace QuirkyBookRental.Controllers
             return View();
         }
 
-       public ActionResult Details(int? id)
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
@@ -53,13 +54,67 @@ namespace QuirkyBookRental.Controllers
             }
             Genre genre = db.Genres.Find(id);
 
-            if(genre == null)
+            if (genre == null)
             {
                 return HttpNotFound();
             }
 
             return View(genre);
-        }    
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(genre);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Genre genre = db.Genres.Find(id);
+            db.Genres.Remove(genre);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
 
         protected override void Dispose(bool disposing)
