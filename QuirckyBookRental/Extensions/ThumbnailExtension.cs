0fe6fcff470c8c15f2c40be6f0288ef3c54648d2
Norewal <63234816+Norewal@users.noreply.github.com>
2022-03-07 18:28:42 +0100

    Create Index View for Home Controller using Thumbnail

---
+++ QuirckyBookRental/Extensions/ThumbnailExtension.cs
@@ -0,0 +1,36 @@
+﻿using QuirkyBookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuirkyBookRental.Extensions
+{
+    public static class ThumbnailExtensions
+    {
+        //it will return a list of thumbnail
+        public static IEnumerable<ThumbnailModel> GetBookThumbnail(this List<ThumbnailModel> thumbnails, ApplicationDbContext db = null)
+        {
+            try
+            {
+                if (db == null) db = ApplicationDbContext.Create();
+
+                thumbnails = (from b in db.Books
+                              select new ThumbnailModel
+                              {
+                                  BookId = b.Id,
+                                  Title = b.Title,
+                                  Description = b.Description,
+                                  ImageUrl = b.ImageUrl,
+                                  Link = "/BookDetail/Index/" + b.Id,
+                              }).ToList(); //converting the output into a list
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return thumbnails.OrderBy(b => b.Title);
+
+        }
+    }
+}
